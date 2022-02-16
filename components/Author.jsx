95459ed0281d0a01267e597d66b6309c1ea77856
Isaac Khan <isaackhan21@gmail.com>
2022-02-16 17:11:18 +0000

    Author and bio

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
 const Author = ({ author }) => {
   return (
-    <div>
-      <img
-        className="rounded-full align-middle"
-        src={author.photo.url}
-        alt={author.name}
-        height="100px"
-        width="100px"
-      />
+    <div className="relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-12 text-center">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          unoptimized
+          className="rounded-full align-middle"
+          src={author.photo.url}
+          alt={author.name}
+          height="100px"
+          width="100px"
+        />
+      </div>
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+      <p className="text-lg text-white">{author.bio}</p>
     </div>
   );
 };
