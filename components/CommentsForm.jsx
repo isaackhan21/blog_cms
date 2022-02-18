48c52ce625580e147951f44923887a50a7b40ce4
Isaac Khan <isaackhan21@gmail.com>
2022-02-18 14:31:43 +0000

    Submit comment functionality and local storage

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,5 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 
+import { submitComment } from "../services";
+
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
   const [localStorage, setlocalStorage] = useState(null);
@@ -9,10 +11,51 @@ const CommentsForm = ({ slug }) => {
   const emailEl = useRef();
   const storeDataEl = useRef();
 
-  const handleCommentSubmission = () => {};
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem("name");
+    emailEl.current.value = window.localStorage.getItem("email");
+  }, []);
+
+  const handleCommentSubmission = () => {
+    setError(false);
+
+    const { value: comment } = commentEl.current;
+    const { value: name } = nameEl.current;
+    const { value: email } = emailEl.current;
+    const { checked: storeData } = storeDataEl.current;
+
+    if (!comment || !name || !email) {
+      setError(true);
+      return;
+    }
+
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    };
+
+    if (storeData) {
+      window.localStorage.setItem("name", name);
+      window.localStorage.setItem("email", email);
+    } else {
+      window.localStorage.removeItem("name", name);
+      window.localStorage.removeItem("email", email);
+    }
+
+    submitComment(commentObj).then((res) => {
+      setshowSuccessMessage(true);
+      setTimeout(() => {
+        setshowSuccessMessage(false);
+      }, 3000);
+    });
+  };
   return (
     <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
-      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">CommentsForm</h3>
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        Leave A Reply
+      </h3>
       <div className="mb-4 grid grid-cols-1 gap-4">
         <textarea
           ref={commentEl}
@@ -37,6 +80,23 @@ const CommentsForm = ({ slug }) => {
           name="email"
         />
       </div>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <div>
+          <input
+            type="checkbox"
+            ref={storeDataEl}
+            id="storeData"
+            name="storeData"
+            value="true"
+          />
+          <label
+            className="ml-2 cursor-pointer text-gray-500"
+            htmlFor="storeData"
+          >
+            Save my email and name for the next time I comment.
+          </label>
+        </div>
+      </div>
       {error && (
         <p className="text-xs text-red-500">All fields are required.</p>
       )}
@@ -48,6 +108,11 @@ const CommentsForm = ({ slug }) => {
         >
           Post Comment
         </button>
+        {showSuccessMessage && (
+          <span className="float-right mt-3 text-xl font-semibold text-green-500">
+            Comment submitted for review
+          </span>
+        )}
       </div>
     </div>
   );
