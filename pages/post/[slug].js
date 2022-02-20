0a02b9394364049a34e3be67a99548c10a207d9f
Isaac Khan <isaackhan21@gmail.com>
2022-02-20 11:39:55 +0000

    Imported Loader to PostDetail, Fallback to true

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useRouter } from "next/router";
+
 import { getPosts, getPostDetails } from "../../services";
 import {
   PostDetail,
@@ -7,9 +9,15 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader,
 } from "../../components";
 
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
@@ -47,6 +55,6 @@ export async function getStaticPaths() {
 
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: false,
+    fallback: true,
   };
 }
